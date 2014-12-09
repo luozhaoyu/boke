6c44586abdbef692d8e200c58168dab57284102d
Zhaoyu Luo <zhaoyu@cs.wisc.edu>
2014-12-09 15:24:08 -0600

    add demo of getting pointer of numpy array

--- demo.py
+++ demo.py
@@ -7,7 +7,7 @@
     A brief description goes here.
 """
 import ctypes
-from ctypes import c_void_p, c_int, c_int, c_char_p
+from ctypes import c_int
 from ctypes import POINTER
 
 import numpy
@@ -28,6 +28,10 @@ def foo():
 
 
 def divide():
+    """
+    vector is not an efficient way to build a list and return to python:
+        http://stackoverflow.com/questions/16885344/how-to-handle-c-return-type-stdvectorint-in-python-ctypes
+    """
     lib = ctypes.cdll.LoadLibrary("./cpp.so")
     lib.divide.restype = POINTER(c_int * 4) # so the result pointer will only iterate 4 times
     lib.divide.argtypes = [c_int, c_int]
@@ -36,8 +40,26 @@ def divide():
         print i
 
 
+def get_numpy_array_pointer():
+    x = numpy.array([3.14, 2.78, 1.4142356])
+    # One should always specify the length of array
+    l = len(x)
+    len_p = c_int(l)
+    # numpy default numerical is double type
+    p = x.ctypes.data_as(POINTER(ctypes.c_double * len_p.value))
+    print p
+    for i in p.contents:
+        print i
+
+    # Besides, convert numpy array to ctypes array
+    p = numpy.ctypeslib.as_ctypes(x)
+    print p
+    for i in p:
+        print i
+
+
 def main(argv):
-    divide()
+    get_numpy_array_pointer()
 
 
 if __name__ == '__main__':
